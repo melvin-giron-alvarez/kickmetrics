9e750e3b7bd06136133c5d5846f52ccb7f6767af
Melvin <melgironalvarez@gmail.com>
2024-07-06 01:10:04 -0700

    modified

--- kickmetrics/vite.config.js
+++ kickmetrics/vite.config.js
@@ -1,7 +1,17 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  build: {
+    outDir: 'build',  // Specify the output directory
+    chunkSizeWarningLimit: 1000, // Adjust the limit as needed
+    rollupOptions: {
+      external: ['firebase/app'],
+      output: {
+        assetFileNames: 'assets/[name]-[hash][extname]',
+        entryFileNames: 'assets/[name]-[hash].js',
+      },
+    },
+  },
+});
