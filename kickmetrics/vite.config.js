ccac3a7d48b6a3ac29353675ccc376e782f8ae8e
Melvin <melgironalvarez@gmail.com>
2024-07-06 01:17:48 -0700

    modified

--- kickmetrics/vite.config.js
+++ kickmetrics/vite.config.js
@@ -6,12 +6,4 @@ export default defineConfig({
   build: {
     outDir: 'build',  // Specify the output directory
     chunkSizeWarningLimit: 1000, // Adjust the limit as needed
-    rollupOptions: {
-      external: ['firebase/app'],
-      output: {
-        assetFileNames: 'assets/[name]-[hash][extname]',
-        entryFileNames: 'assets/[name]-[hash].js',
-      },
-    },
-  },
 });
